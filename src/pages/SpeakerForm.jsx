59255d58eb220587e9cf240d9a311f16bec6ab6c
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-08-12 18:18:27 +0530

    Update SpeakerForm.jsx

--- src/pages/SpeakerForm.jsx
+++ src/pages/SpeakerForm.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import backgroundVideo from "../assets/background.mp4";
 import "./SpeakerForm.css";
 import FooterSection from "../sections/FooterSection";
+import { FiExternalLink } from "react-icons/fi";
+
 
 const SpeakerForm = () => {
   const [formData, setFormData] = useState({
@@ -73,7 +75,33 @@ const SpeakerForm = () => {
         Your browser does not support the video tag.
       </video>
       <main className="form-page">
+        <div className="para">
+            <h2>Theme for TEDxPVGCOET 2025-2026:</h2><br/><br/>
+
+            <p>The theme for <strong>TEDxPVGCOET 2025</strong> is  <strong>"Drishti (दृष्टि)"</strong>, a Sanskrit word that means vision or perspective. But this isn’t just about what we see with our eyes—it’s about how we understand, interpret, and connect with the world around us. Drishti is about looking beyond the surface to find deeper meaning and clarity.<br/><br/>
+
+            In a fast-paced world full of noise and distraction, Drishti asks us to slow down, reflect, and truly see—ourselves, others, and the challenges we face. It’s about shifting perspective, finding insight in unexpected places, and discovering new ways of thinking, feeling, and acting. Whether in our personal lives, work, or relationships, this kind of vision leads to transformation.<br/><br/>
+
+            For more details about the theme check out this document:<br/>
+            <a
+              href="https://docs.google.com/document/d/1Sw1Fh00eBpIFEEyjzYP6uHSiJABevaSqhiyfg0tmFh8/edit?usp=drivesdk"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white"}}
+            >
+              Theme Document&nbsp;
+              <FiExternalLink size={12} />
+            </a><br/><br/>
+            
+
+            
+            <strong> Date:</strong> September 13th, 2025&nbsp;&nbsp;
+            <strong> Location:</strong> PVGCOET, Pune, Maharashtra, India<br/>
+          </p>
+          </div>
         <div className="form-inner">
+          
+        
           <h1>Speaker Nomination</h1>
           <form className="speaker-form" onSubmit={handleSubmit}>
             <div className="full-width">
@@ -117,8 +145,85 @@ const SpeakerForm = () => {
               />
             </div>
 
+           <div className="full-width">
+              <input
+                  type="text"
+                  name="organization"
+                  placeholder="Current Location/City"
+                  required
+                  value={formData.organization}
+                  onChange={handleChange}
+                />
+            </div>
+
+            <div className="full-width">
+              <input
+                  type="text"
+                  name="organization"
+                  placeholder="Profesional Affiliation/Title"
+                  required
+                  value={formData.organization}
+                  onChange={handleChange}
+                />
+            </div>
+            
+
+            
+
             <div className="full-width">
               <h3>How do you think your talk will impact or inspire the audience?</h3>
+              <br />
+              <input
+                type="text"
+                name="audience_impact"
+                placeholder="Answer"
+                required
+                value={formData.audience_impact}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="full-width">
+              <h3>Speaker's Bio (Briefly describe your professional background and current occupation)</h3>
+              <br />
+              <input
+                type="text"
+                name="audience_impact"
+                placeholder="Answer"
+                required
+                value={formData.audience_impact}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="full-width">
+              <h3>Have you given a TED or TEDx talk before? If yes, please provide details.</h3>
+              <br />
+              <input
+                type="text"
+                name="audience_impact"
+                placeholder="Answer"
+                required
+                value={formData.audience_impact}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="full-width">
+              <h3>How will your talk align with this year's TEDxPVGCOET theme "Drishti"?</h3>
+              <br />
+              <input
+                type="text"
+                name="audience_impact"
+                placeholder="Answer"
+                required
+                value={formData.audience_impact}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="full-width">
+              <h3>Any Additional Comments or Information:</h3>
               <br />
               <input
                 type="text"
