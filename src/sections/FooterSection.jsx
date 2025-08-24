7f6751f2f1eb8f4b01f34d7ffcf95385a225ce96
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-08-24 21:27:00 +0530

    Allign twitter (#31)
    * add twitter link at footer and fix allignment to type of partership
    
    * done work of twitter and allignmnet
    
    ---------
    
    Co-authored-by: Hussain Patanwala <hussainpatanwala6@gmail.com>

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./FooterSection.css";
 import { Link } from "react-router-dom";
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const FooterSection = () => {
   return (
@@ -18,8 +19,9 @@ const FooterSection = () => {
             This independent TEDx event is operated under licence from TED.
           </p>
           <div className="footer-icons">
-            <a href="https://www.instagram.com/tedxpvgcoet/?hl=en"><FaInstagram /></a>
-            <a href="https://www.linkedin.com/company/tedxpvgcoet/?originalSubdomain=in"><FaLinkedin /></a>
+            <a href="https://www.instagram.com/tedxpvgcoet/?hl=en" target="_blank"><FaInstagram /></a>
+            <a href="https://www.linkedin.com/company/tedxpvgcoet/?originalSubdomain=in" target="_blank"><FaLinkedin /></a>
+            <a href="https://x.com/TEDxPVGCOET" target="_blank"><FaXTwitter/></a>
           </div>
         </div>
 
